764401451cae658c288f9847e785c71ce69171e6
azmanvai <32783505+azmanvai@users.noreply.github.com>
2017-11-14 13:34:54 +0800

    Add files via upload
    Update final line to display all result

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@ run_analysis <- function() {
           library(dplyr)
           library(tidyr)
           
-          ## Read and tidy train data set
+          ## Section 1: Read and tidy train data set
           variables <- read.table("features.txt", header = F) ## table of 561 variable names and ids
           activity_labels <- read.table("activity_labels.txt", header = F) ## table of 6 activity types and ids
           x_train <- read.table("./train/X_train.txt", header = F) ## table of 7352 observations of 561 variables
@@ -19,7 +19,7 @@ run_analysis <- function() {
           x_train_selected <- x_train[ , selected_variables] ## subset data frame based on selected 79 variables with mean or std
           x_train_merged <- cbind(x_train_selected, y_train, subject_train) ## bind variables for all observations with activity id and subject_id
 
-          ## Read and tidy test data set
+          ## Section 2: Read and tidy test data set
           x_test <- read.table("./test/X_test.txt", header = F) ## table of 2947 observations of 561 variables
           y_test <- read.table("./test/y_test.txt", header = F) ## table of activity ids for 2947 observations
           subject_test <- read.table("./test/subject_test.txt", header = F) ## table of subject ids for 2947 observations
@@ -31,13 +31,13 @@ run_analysis <- function() {
           x_test_selected <- x_test[ , selected_variables] ## subset data frame based on selected 79 variables with mean or std
           x_test_merged <- cbind(x_test_selected, y_test, subject_test) ## bind variables for all observations with activity id and subject_id
 
-          ## Combine rows of train and test data sets
+          ## Section 3: Combine rows of train and test data sets
           all_data <- rbind(x_train_merged, x_test_merged)  ## bind x_train and x_test by row
           all_data_merged <- merge(all_data, activity_labels, by="activity_id" ) ## merge with activity_labels to get activity types
           all_data_gathered <- gather(all_data_merged, "measure", "value", 2:80) ## gather all variables under measure column
           all_data_gathered <- select(all_data_gathered, subject_id:value) ## select all columns except activity_id
        
-          return(head(all_data_gathered,50)) ## display top 50 of tidy data
+          return(all_data_gathered) ## display result of tidy data
 }
 
 
